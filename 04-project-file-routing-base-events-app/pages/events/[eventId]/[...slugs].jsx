e87947c8be604eec3d152687c543e6d3d26c55ec
danielOuattara <daniel.ouattara@gmx.com>
2023-05-03 19:11:42 +0200

    04 Starting project file route base - Structure

---
+++ 04-project-file-routing-base-events-app/pages/events/[eventId]/[...slugs].jsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+export default function FilteredEventPage() {
+  const router = useRouter();
+  console.log("router blog = ", router);
+  return (
+    <div>
+      <h1>Details for </h1>
+    </div>
+  );
+}
