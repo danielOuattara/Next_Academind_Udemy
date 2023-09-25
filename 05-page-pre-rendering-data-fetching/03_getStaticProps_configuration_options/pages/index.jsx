acf6681849291c362b31bf39f1d187e3b60a9d18
danielOuattara <daniel.ouattara@gmx.com>
2023-09-25 20:52:04 +0200

    05 03 getStaticProps configuration options

---
+++ 05-page-pre-rendering-data-fetching/03_getStaticProps_configuration_options/pages/index.jsx
@@ -0,0 +1,49 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+import * as fs from "node:fs/promises";
+import path from "path";
+
+export default function Home(props) {
+  console.log("props = ", props);
+  return (
+    <ul>
+      {props.products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps(context) {
+  const filePath = path.join(process.cwd(), "data", "mock-data.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse([jsonData]);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  // const data = { products: [] }; // for testing
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    // notFound: true,
+    // redirect: {
+    //   destination: "/no-data",
+    // },
+    revalidate: 3,
+  };
+}
