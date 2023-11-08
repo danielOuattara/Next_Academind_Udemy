9c87bed67b84f0c8af57a924e50040ed44a2742c
danielOuattara <daniel.ouattara@gmx.com>
2023-11-08 17:17:19 +0100

    116 combining pre-fetching & client-side fetching

--- 06-Client-Side-Data-Fetching/pages/last-sales-swr.jsx
+++ 06-Client-Side-Data-Fetching/pages/last-sales-swr.jsx
@@ -1,9 +1,10 @@
 import useSWR from "swr";
+import { useEffect, useState } from "react";
 
 const url = `https://acadmind-nextjs-client-fetch-default-rtdb.europe-west1.firebasedatabase.app/sales.json`;
 
 const fetcher = async () => {
-  return fetch(url)
+  fetch(url)
     .then((res) => res.json())
     .then((data) => {
       const sales = [];
@@ -17,21 +18,44 @@ const fetcher = async () => {
       return sales;
     });
 };
-export default function LastSales() {
+
+//-------------------------------------
+export default function LastSales(props) {
+  const [sales, setSales] = useState(props.sales);
+
   const response = useSWR(url, fetcher);
-  const { data, error, isLoading } = response;
+
+  const { data, error } = response;
+
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+      for (const key in data) {
+        sales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
 
   if (error) {
     return <h1>Error: ${error.message}...</h1>;
   }
 
-  if (isLoading) {
+  if (!data && !sales) {
     return <h1>Loading...</h1>;
+  }
+
+  if (data) {
+    setSales(data);
   }
 
   return (
     <ul>
-      {data.map((item) => (
+      {sales.map((item) => (
         <li key={item.id}>
           <h2>
             {item.username} - {item.volume}
@@ -40,4 +64,24 @@ export default function LastSales() {
       ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const sales = [];
+      for (const key in data) {
+        sales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      return {
+        props: {
+          sales,
+        },
+      };
+    });
 }
