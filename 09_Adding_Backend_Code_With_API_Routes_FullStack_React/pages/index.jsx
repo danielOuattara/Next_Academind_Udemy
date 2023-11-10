3d23643f63242fb16e59ac40eb6ac167676a71af
danielOuattara <daniel.ouattara@gmx.com>
2023-11-10 12:12:37 +0100

    145 preparing the frontend

--- 09_Adding_Backend_Code_With_API_Routes_FullStack_React/pages/index.jsx
+++ 09_Adding_Backend_Code_With_API_Routes_FullStack_React/pages/index.jsx
@@ -1,9 +1,37 @@
-function HomePage() {
+import { useRef } from "react";
+
+export default function HomePage() {
+  const emailInput = useRef();
+  const feedbackInput = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const emailValue = emailInput.current.value;
+    const feedbackValue = feedbackInput.current.value;
+  };
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form action="" method="get" onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="email">Your email Address :</label>
+          <input type="email" id="email" name="email" ref={emailInput} />
+        </div>
+        <hr />
+        <div>
+          <label htmlFor="feedback">Your Feedback :</label>
+          <textarea
+            name="feedback"
+            id="feedback"
+            cols="30"
+            rows="10"
+            ref={feedbackInput}
+          ></textarea>
+        </div>
+
+        <button type="submit">Send feedback</button>
+      </form>
     </div>
   );
 }
-
-export default HomePage;
