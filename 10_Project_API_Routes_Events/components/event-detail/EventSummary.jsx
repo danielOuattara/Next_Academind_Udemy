0dc555892eea61a3884b9ae226cdced239b8b7c5
danielOuattara <daniel.ouattara@gmx.com>
2023-11-30 20:40:51 +0100

    project folder re-organized

--- 10_Project_API_Routes_Events/components/event-detail/EventSummary.jsx
+++
@@ -1,9 +0,0 @@
-import classes from "./event-summary.module.css";
-
-export default function EventSummary(props) {
-  return (
-    <section className={classes.summary}>
-      <h1>{props.title}</h1>
-    </section>
-  );
-}
