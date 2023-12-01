93aab52c531c62b880929624d40b45bb646b207a
danielOuattara <daniel.ouattara@gmx.com>
2023-12-01 12:43:11 +0100

    another error handling logic

--- 10_Project_API_Routes_Events/max_solution/pages/api/newsletter-registration/index.js
+++ 10_Project_API_Routes_Events/max_solution/pages/api/newsletter-registration/index.js
@@ -1,37 +1,33 @@
 import { MongoClient } from "mongodb";
 
+async function connectToDatabase() {
+  return await MongoClient.connect(process.env.MONGO_URI);
+}
+
+async function insertToDocuments(client, data) {
+  const db = client.db();
+  await db.collection("emails").insertOne(data);
+}
+
 export default async function handler(req, res) {
-  try {
-    if (req.method === "POST") {
-      // email validation here too !
-      if (
-        !req.body.email ||
-        req.body.email.trim() === "" ||
-        !req.body.email.includes("@")
-      ) {
-        throw new Error("Invalid email address");
-      }
+  if (req.method === "POST") {
+    // email validation here too !
+    if (
+      !req.body.email ||
+      req.body.email.trim() === "" ||
+      !req.body.email.includes("@")
+    ) {
+      return res.status(422).json({ message: "Invalid email address" });
+    }
 
-      // const client = new MongoClient(process.env.MONGO_URI);
-      // await client.connect();
-      // const db = client.db();
-      // await db.collection("emails").insertOne({ email: req.body.email });
-      // client.close();
-
-      // OR
-
-      const client = await MongoClient.connect(process.env.MONGO_URI);
-      const db = client.db();
-      await db.collection("emails").insertOne({ email: req.body.email });
+    try {
+      const client = await connectToDatabase();
+      await insertToDocuments(client, { email: req.body.email });
       client.close();
-
       return res.status(201).json({ message: "success !" });
-    }
-  } catch (error) {
-    console.log(error);
-    if (error instanceof MongoServerError) {
+    } catch (error) {
       console.log(`Mongo Error: ${error.message}`); // special case for some reason
+      return res.status(500).json({ message: `Mongo Error: ${error.message}` });
     }
-    return res.status(422).json({ message: error.message });
   }
 }
