50ea94d3dd9a0ec4a5d5d0735471f7febf5a74c6
danielOuattara <daniel.ouattara@gmx.com>
2023-12-05 16:34:33 +0100

    adding try catch and using utilities functions

--- 10_Project_API_Routes_Events/max_solution/pages/api/comments/[eventId].js
+++ 10_Project_API_Routes_Events/max_solution/pages/api/comments/[eventId].js
@@ -1,4 +1,9 @@
 import { MongoClient } from "mongodb";
+import {
+  connectToDatabase,
+  insertToDocuments,
+  getDocumentData,
+} from "@/utilities/database-utility";
 
 export default async function handler(req, res) {
   const eventId = req.query.eventId;
@@ -7,47 +12,46 @@ export default async function handler(req, res) {
   const db = client.db();
 
   if (req.method === "POST") {
+    const { email, name, comment } = req.body;
+
+    // data validation
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !comment ||
+      comment.trim() === ""
+    ) {
+      return res.status(422).json({ message: "Invalid inputs" });
+    }
+
     try {
-      const { email, name, comment } = req.body;
-
-      // data validation
-      if (
-        !email ||
-        !email.includes("@") ||
-        !name ||
-        name.trim() === "" ||
-        !comment ||
-        comment.trim() === ""
-      )
-        throw new Error("invalid input");
-
-      await db.collection("comments").insertOne({
+      const client = await connectToDatabase();
+      await insertToDocuments(client, "comments", {
         eventId,
         email,
         name,
         comment,
       });
 
-      client.close();
-
       return res.status(201).json({
         message: "commented successfully !",
       });
     } catch (error) {
-      console.log(error);
-      if (error instanceof MongoServerError) {
-        console.log(`Mongo Error: ${error.message}`); // special case for some reason
-      }
-      return res.status(422).json({ message: error.message });
+      console.log(`Mongo Error: ${error.message}`); // special case for some reason
+      return res.status(500).json({ message: `Mongo Error: ${error.message}` });
     }
   }
   if (req.method === "GET") {
-    const commentList = await db
-      .collection("comments")
-      .find({ eventId: eventId })
-      .sort({ _id: -1 })
-      .toArray();
-    client.close();
-    res.status(200).json({ comments: commentList });
+    try {
+      const commentList = await getDocumentData(client, "comments", {
+        eventId,
+      });
+      res.status(200).json({ comments: commentList });
+    } catch (error) {
+      console.log(`Mongo Error: ${error.message}`); // special case for some reason
+      return res.status(500).json({ message: `Mongo Error: ${error.message}` });
+    }
   }
 }
