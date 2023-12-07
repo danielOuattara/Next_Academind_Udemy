1a1cc71f6c29c6c4b51297de718574374854a8d4
danielOuattara <daniel.ouattara@gmx.com>
2023-12-07 11:37:33 +0100

    03:02: using Suspense and Loading locally

--- 03_NextJS_Essentials_App_Router/02_foodies_project/app/meals/page.jsx
+++ 03_NextJS_Essentials_App_Router/02_foodies_project/app/meals/page.jsx
@@ -2,9 +2,15 @@ import MealsGrid from "@/components/meals/meals-grid";
 import styles from "./page.module.css";
 import Link from "next/link";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+import Loading from "./loading";
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={styles.header}>
@@ -22,7 +28,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<Loading />}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
