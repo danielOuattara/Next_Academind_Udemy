1f4e512d79e1088960884368962fa9754592d895
danielOuattara <daniel.ouattara@gmx.com>
2023-12-08 14:35:48 +0100

    02: 02: adding empty postsList message

--- 02_React_Refresher/02_next_app_router_version/_02_react_app/src/components/PostsList.jsx
+++ 02_React_Refresher/02_next_app_router_version/_02_react_app/src/components/PostsList.jsx
@@ -21,11 +21,20 @@ export default function PostList(props) {
           />
         </Modal>
       )}
-      <ul className={styles.posts}>
-        {postsList.map((post) => (
-          <Post key={post.id} {...post} />
-        ))}
-      </ul>
+
+      {postsList.length === 0 && (
+        <h2 style={{ textAlign: "center" }}>
+          No post yet. Please add new post
+        </h2>
+      )}
+
+      {postsList.length !== 0 && (
+        <ul className={styles.posts}>
+          {postsList.map((post) => (
+            <Post key={post.id} {...post} />
+          ))}
+        </ul>
+      )}
     </>
   );
 }
