e1c9778e1cfcab65a239b93cb5dc7e1214246a07
danielOuattara <daniel.ouattara@gmx.com>
2023-12-13 17:08:04 +0100

    02:02: routing: adding create-post route

--- 02_React_Refresher/02_next_app_router_version/_02_react_app/frontend-react-vite/src/main.jsx
+++ 02_React_Refresher/02_next_app_router_version/_02_react_app/frontend-react-vite/src/main.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+import NewPost from "./components/NewPost";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [],
+  },
+  {
+    path: "/create-post",
+    element: <NewPost />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
