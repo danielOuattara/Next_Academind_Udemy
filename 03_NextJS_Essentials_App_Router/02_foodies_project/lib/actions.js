eb86ddfc2f34399f4f364c70f81650fe5fb51120
danielOuattara <daniel.ouattara@gmx.com>
2023-12-13 08:53:52 +0100

    03:02: input validation server side

--- 03_NextJS_Essentials_App_Router/02_foodies_project/lib/actions.js
+++ 03_NextJS_Essentials_App_Router/02_foodies_project/lib/actions.js
@@ -4,15 +4,37 @@ import { saveMeal } from "./meals";
 
 // "use server" creates a "server action", a function guarantied to work ONLY on server
 
+function isInvalidText(text) {
+  if (!text || text.trim() === "") {
+    return true;
+  }
+  return false;
+}
+
 export async function shareMeal(formData) {
   const meal = {
     title: formData.get("title"),
     summary: formData.get("summary"),
-    instruction: formData.get("instructions"),
+    instructions: formData.get("instructions"),
     image: formData.get("image"),
     creator: formData.get("creator"),
     creator_email: formData.get("email"),
   };
+
+  // input validation "server side"
+
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    throw new Error("Error in Creating new Meal: inputs are not valid");
+  }
 
   await saveMeal(meal);
   redirect("/meals");
