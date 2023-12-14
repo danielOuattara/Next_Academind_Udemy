2286185461bd1834f76771b3d1e4a2c46ca61083
danielOuattara <daniel.ouattara@gmx.com>
2023-12-14 10:55:39 +0100

    02:02: routing: add post details + update logic

--- 02_React_Refresher/02_next_app_router_version/_02_react_app/frontend-react-vite/src/main.jsx
+++ 02_React_Refresher/02_next_app_router_version/_02_react_app/frontend-react-vite/src/main.jsx
@@ -2,21 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
-import NewPost from "./routes/NewPost";
+import NewPost, { action as submitPost } from "./routes/NewPost";
 import RootLayout from "./routes/RootLayout";
-import Posts from "./routes/Posts";
-import { loader as fetchPostData } from "./routes/Posts";
-
+import Posts, { loader as fetchPostsData } from "./routes/Posts";
+import ErrorPage from "./components/error-page";
+import PostDetails, { loader as fetchSinglePost } from "./routes/PostDetails";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
         element: <Posts />,
-        loader: fetchPostData,
-        children: [{ path: "create-post", element: <NewPost /> }],
+        loader: fetchPostsData,
+        children: [
+          { path: "/create-post", element: <NewPost />, action: submitPost },
+          {
+            path: "/:postId",
+            element: <PostDetails />,
+            loader: fetchSinglePost,
+          },
+        ],
       },
     ],
   },
