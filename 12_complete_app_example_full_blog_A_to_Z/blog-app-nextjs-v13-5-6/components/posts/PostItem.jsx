b0ee8945b71ad9f5dc656c6dd11c37ad53238484
danielOuattara <daniel.ouattara@gmx.com>
2023-12-14 13:28:47 +0100

    12: blog: blog v13&v14: adding PostGrid & PostItem

--- 12_complete_app_example_full_blog_A_to_Z/blog-app-nextjs-v13-5-6/components/posts/PostItem.jsx
+++ 12_complete_app_example_full_blog_A_to_Z/blog-app-nextjs-v13-5-6/components/posts/PostItem.jsx
@@ -1,5 +1,29 @@
 import styles from "./PostItem.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
-export default function PostItem() {
-  return <div>PostItem</div>;
+export default function PostItem(props) {
+  const formattedDate = new Date(props.date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const imagePath = `/images/posts/${props.slug}/${props.image}`;
+
+  return (
+    <li className={styles.post}>
+      <Link href={props.postSlug}>
+        <div className={styles.image}>
+          <Image src={imagePath} alt={props.title} width={300} height={200} />
+        </div>
+
+        <div className={styles.content}>
+          <h3>Title {props.title}</h3>
+          <time>time data: {formattedDate}</time>
+          <p>The excerpt {props.excerpt}</p>
+        </div>
+      </Link>
+    </li>
+  );
 }
