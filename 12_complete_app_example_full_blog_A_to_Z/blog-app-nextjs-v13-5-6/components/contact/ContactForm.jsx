28720c0914de88f2528566f4022ef4d972438294
danielOuattara <daniel.ouattara@gmx.com>
2023-12-29 13:19:19 +0100

    12 blog v13 ONLY: storing contact messages using MongoDb

--- 12_complete_app_example_full_blog_A_to_Z/blog-app-nextjs-v13-5-6/components/contact/ContactForm.jsx
+++ 12_complete_app_example_full_blog_A_to_Z/blog-app-nextjs-v13-5-6/components/contact/ContactForm.jsx
@@ -1,10 +1,22 @@
 import styles from "./ContactForm.module.css";
 
 export default function ContactForm() {
+  const sendMessageHandler = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    fetch("/api/contact/", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(Object.fromEntries(formData)),
+    });
+  };
+
   return (
     <section className={styles.contact}>
       <h1>How can I help you ?</h1>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={sendMessageHandler}>
         <div className={styles.controls}>
           <div className={styles.control}>
             <label htmlFor="email">Your email</label>
@@ -26,3 +38,46 @@ export default function ContactForm() {
     </section>
   );
 }
+
+//----------------------------------------------------------------------
+
+// import styles from "./ContactForm.module.css";
+
+// export default function ContactForm() {
+//   const sendMessageHandler = (event) => {
+//     event.preventDefault();
+//     const formData = new FormData(event.currentTarget);
+//     fetch("/api/contact/", {
+//       method: "POST",
+//       headers: {
+//         "content-type": "multipart/form-data",
+//       },
+//       body: formData,
+//     });
+//   };
+
+//   return (
+//     <section className={styles.contact}>
+//       <h1>How can I help you ?</h1>
+//       <form className={styles.form} onSubmit={sendMessageHandler}>
+//         <div className={styles.controls}>
+//           <div className={styles.control}>
+//             <label htmlFor="email">Your email</label>
+//             <input type="email" id="email" name="email" required />
+//           </div>
+//           <div className={styles.control}>
+//             <label htmlFor="name">Your name</label>
+//             <input type="text" id="name" name="name" required />
+//           </div>
+//         </div>
+//         <div className={styles.control}>
+//           <label htmlFor="message">Your message</label>
+//           <textarea name="message" id="message" rows="6"></textarea>
+//         </div>
+//         <div className={styles.actions}>
+//           <button>Send Message</button>
+//         </div>
+//       </form>
+//     </section>
+//   );
+// }
--- 12_complete_app_example_full_blog_A_to_Z/blog-app-nextjs-v13-5-6/pages/api/contact.js
+++ 12_complete_app_example_full_blog_A_to_Z/blog-app-nextjs-v13-5-6/pages/api/contact.js
@@ -1,10 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
-
-    // data validation
+    // quick data validation
     if (
       !email ||
       !email.includes("@") ||
@@ -13,11 +14,65 @@ export default function handler(req, res) {
       !message ||
       message.trim() === ""
     ) {
-      res.status(422).json({ message: "Invalid input" });
+      return res.status(422).json({ message: "Invalid input" });
     }
-  }
-  const newMessage = { email, name, message };
-  console.log(newMessage);
 
-  res.status(201).json({ message: "Success", newMessage });
+    let client;
+
+    try {
+      client = await MongoClient.connect(process.env.MONGO_URI);
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+
+    const db = client.db();
+
+    let result;
+
+    try {
+      result = await db
+        .collection("form_messages")
+        .insertOne({ email, name, message });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+
+    client.close();
+
+    return res.status(201).json({
+      server_message: "Success",
+      id: result.insertedId,
+      email,
+      message,
+    });
+  }
 }
+
+//---------------------------------------------------------------
+
+// import { NextApiRequest, NextApiResponse } from "next";
+
+// export default async function handler(req, res) {
+//   console.log("req = ", req);
+//   if (req.method === "POST") {
+//     const formData = await req.formData();
+//     const name = formData.get("name");
+//     const email = formData.get("email");
+//     const message = formData.get("message");
+
+//     // data validation
+//     if (
+//       !email ||
+//       // !email.includes("@") ||
+//       !name ||
+//       name.trim() === "" ||
+//       !message ||
+//       message.trim() === ""
+//     ) {
+//       res.status(422).json({ message: "Invalid input" });
+//     }
+//     const newMessage = { email, name, message };
+//     console.log(newMessage);
+//     return res.status(201).json({ message: "Success", newMessage });
+//   }
+// }
