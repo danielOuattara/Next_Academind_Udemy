be19b653635ef6836e2c879e32d801f089964d42
danielOuattara <daniel.ouattara@gmx.com>
2023-12-29 15:11:12 +0100

    12 blog v13 ONLY: UI notification feedback

---
+++ 12_complete_app_example_full_blog_A_to_Z/blog-app-nextjs-v13-5-6/components/ui/Notification.jsx
@@ -0,0 +1,22 @@
+import styles from "./Notification.module.css";
+
+export default function Notification(props) {
+  let statusClasses = "";
+
+  if (props.status === "success") {
+    statusClasses = styles.success;
+  }
+
+  if (props.status === "error") {
+    statusClasses = styles.error;
+  }
+
+  const cssClasses = `${styles.notification} ${statusClasses}`;
+
+  return (
+    <div className={cssClasses}>
+      <h2>{props.title}</h2>
+      <p>{props.message}</p>
+    </div>
+  );
+}
