97699b19eaf045c1d1ba0a9cccccd06c085fdb36
danielOuattara <daniel.ouattara@gmx.com>
2023-12-29 04:58:21 +0100

    12 blog v13 & v14 Rendering Code Snippets From markdown

--- 12_complete_app_example_full_blog_A_to_Z/blog-app-nextjs-v13-5-6/components/posts/post-details/PostContent.jsx
+++ 12_complete_app_example_full_blog_A_to_Z/blog-app-nextjs-v13-5-6/components/posts/post-details/PostContent.jsx
@@ -2,6 +2,8 @@ import styles from "./PostContent.module.css";
 import PostHeader from "./PostHeader";
 import Markdown from "react-markdown";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark, dark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 export default function PostContent(props) {
   const customRenderers = {
@@ -37,6 +39,38 @@ export default function PostContent(props) {
       } else {
         return <p>{paragraph.children}</p>;
       }
+    },
+
+    /* -----From docs ----- */
+    // code(props) {
+    //   const { children, className, node, ...rest } = props;
+    //   const match = /language-(\w+)/.exec(className || "");
+    //   return match ? (
+    //     <SyntaxHighlighter
+    //       {...rest}
+    //       PreTag="div"
+    //       children={String(children).replace(/\n$/, "")}
+    //       language={match[1]}
+    //       style={atomDark}
+    //     />
+    //   ) : (
+    //     <code {...rest} className={className}>
+    //       {children}
+    //     </code>
+    //   );
+    // },
+
+    /* -----From Max ----- */
+    code(props) {
+      const { className, children } = props;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
     },
   };
 
--- 12_complete_app_example_full_blog_A_to_Z/blog-app-nextjs-v14-0-4/components/posts/post-details/PostContent.jsx
+++ 12_complete_app_example_full_blog_A_to_Z/blog-app-nextjs-v14-0-4/components/posts/post-details/PostContent.jsx
@@ -2,6 +2,8 @@ import styles from "./PostContent.module.css";
 import PostHeader from "./PostHeader";
 import Markdown from "react-markdown";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark, dark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 export default function PostContent(props) {
   const customRenderers = {
@@ -38,7 +40,40 @@ export default function PostContent(props) {
         return <p>{paragraph.children}</p>;
       }
     },
+
+    /* -----From docs ----- */
+    // code(props) {
+    //   const { children, className, node, ...rest } = props;
+    //   const match = /language-(\w+)/.exec(className || "");
+    //   return match ? (
+    //     <SyntaxHighlighter
+    //       {...rest}
+    //       PreTag="div"
+    //       children={String(children).replace(/\n$/, "")}
+    //       language={match[1]}
+    //       style={atomDark}
+    //     />
+    //   ) : (
+    //     <code {...rest} className={className}>
+    //       {children}
+    //     </code>
+    //   );
+    // },
+
+    /* -----From Max ----- */
+    code(props) {
+      const { className, children } = props;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
+    },
   };
+
   return (
     <article className={styles.content}>
       <PostHeader
