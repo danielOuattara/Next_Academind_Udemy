9efa735579dd8b1f0f4d0f101118155cba288407
danielOuattara <daniel.ouattara@gmx.com>
2024-01-03 04:42:21 +0100

    14  adding credential Auth Provider & Uer login logic

---
+++ 14_Adding_Next_Auth/next-js-v13.5.3/pages/api/auth/[...nextauth].js
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "../../../libraries/db_lib";
+import { compare } from "bcryptjs";
+
+export const authOptions = {
+  session: {
+    // strategy: "jwt",
+    jwt: true,
+  },
+
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      async authorize(credentials, req) {
+        const client = await connectToDatabase();
+
+        const user = await client
+          .db()
+          .collection("users")
+          .findOne({ email: credentials.email });
+
+        if (!user) {
+          client.close();
+          throw new Error("Email or Password invalid !");
+        }
+
+        const isValidPassword = await compare(
+          credentials.password,
+          user.password,
+        );
+
+        if (!isValidPassword) {
+          client.close();
+          throw new Error("Email or Password invalid !!");
+        }
+
+        client.close();
+        return { email: user.email };
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
