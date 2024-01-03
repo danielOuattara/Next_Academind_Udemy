aa4c2ddd61a96af8d3ad674492ee6684a8bff5eb
danielOuattara <daniel.ouattara@gmx.com>
2024-01-03 15:54:43 +0100

    14 Adding Server-Side Page Guards (And When To Use Which Approach)

--- 14_Adding_Next_Auth/next-js-v13.5.3/components/profile/user-profile.jsx
+++ 14_Adding_Next_Auth/next-js-v13.5.3/components/profile/user-profile.jsx
@@ -27,29 +27,44 @@
 //---------------------------------------------------------------
 
 /* 2 */
+// import ProfileForm from "./profile-form";
+// import styles from "./user-profile.module.css";
+// import { getSession } from "next-auth/react";
+// import { useEffect, useState } from "react";
+
+// export default function UserProfile() {
+//   // Redirect away if NOT auth
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   useEffect(() => {
+//     getSession().then((session) => {
+//       if (!session) {
+//         window.location.href = "/auth";
+//       } else {
+//         setIsLoading(false);
+//       }
+//     });
+//   }, []);
+
+//   if (isLoading) {
+//     return <h1 className={styles.profile}>Loading...</h1>;
+//   }
+
+//   return (
+//     <section className={styles.profile}>
+//       <h1>Your User Profile</h1>
+//       <ProfileForm />
+//     </section>
+//   );
+// }
+
+//---------------------------------------------------------------
+
+/* 3 */
 import ProfileForm from "./profile-form";
 import styles from "./user-profile.module.css";
-import { getSession } from "next-auth/react";
-import { useEffect, useState } from "react";
 
 export default function UserProfile() {
-  // Redirect away if NOT auth
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    getSession().then((session) => {
-      if (!session) {
-        window.location.href = "/auth";
-      } else {
-        setIsLoading(false);
-      }
-    });
-  }, []);
-
-  if (isLoading) {
-    return <h1 className={styles.profile}>Loading...</h1>;
-  }
-
   return (
     <section className={styles.profile}>
       <h1>Your User Profile</h1>
--- 14_Adding_Next_Auth/next-js-v13.5.3/pages/profile.jsx
+++ 14_Adding_Next_Auth/next-js-v13.5.3/pages/profile.jsx
@@ -1,5 +1,24 @@
 import UserProfile from "../components/profile/user-profile";
+import { getSession } from "next-auth/react";
 
-export default function ProfilePage() {
+export default function ProfilePage(props) {
+  console.log(props);
   return <UserProfile />;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
 }
