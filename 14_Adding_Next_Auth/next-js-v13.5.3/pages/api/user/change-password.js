12a809219dc83140fdf81eea52b980a9e39f9dfd
danielOuattara <daniel.ouattara@gmx.com>
2024-01-03 19:59:45 +0100

    14  Adding the "Change Password" Logic

--- 14_Adding_Next_Auth/next-js-v13.5.3/pages/api/user/change-password.js
+++ 14_Adding_Next_Auth/next-js-v13.5.3/pages/api/user/change-password.js
@@ -1,9 +1,51 @@
 import { getSession } from "next-auth/react";
+import { connectToDatabase } from "../../../libraries/db_lib";
+import { compare, hash } from "bcryptjs";
 
 export default async function handler(req, res) {
   if (req.method !== "PATCH") return;
 
   const session = await getSession({ req });
-
   if (!session) return res.status(401).json({ message: "Not Authorized !" });
+
+  if (
+    !req.body.oldPassword ||
+    req.body.oldPassword.trim().length < 3 ||
+    !req.body.password ||
+    req.body.password.trim().length < 3
+  ) {
+    return res.status(422).json({ message: "Invalid email or password" });
+  }
+
+  const client = await connectToDatabase();
+  const user = await client
+    .db()
+    .collection("users")
+    .findOne({ email: session.user.email });
+
+  if (!user) {
+    client.close();
+    return res.status(403).json({ message: "Not Authorized !" });
+  }
+
+  const isValidPassword = await compare(req.body.oldPassword, user.password);
+  if (!isValidPassword) {
+    client.close();
+    return res.status(403).json({ message: "Not Authorized !" });
+  }
+
+  const newHashedPwd = await hash(req.body.newPassword, 13);
+
+  const result = await client
+    .db()
+    .collection("users")
+    .updateOne(
+      { email: session.user.email },
+      { $set: { password: newHashedPwd } },
+    );
+
+  console.log("result = ", result);
+
+  client.close();
+  return res.status(201).json({ message: "Password updated successfully" });
 }
